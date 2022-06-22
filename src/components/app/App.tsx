c195cf9d7ad6ea7efbba283b029d11ee4d20e838
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-22 14:41:53 +0200

    reorganize components and cleaning structure

---
+++ src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import Footer from '../footer/Footer';
+import Header from '../header/Header';
+import Main from '../main/Main';
+
+const App: FC = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
