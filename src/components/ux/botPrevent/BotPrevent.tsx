694cb146fd85cfcee4b4f31394bd16c097068f4b
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-23 00:05:10 +0200

    add basic answers shuffle

--- src/components/ux/botPrevent/BotPrevent.tsx
+++ src/components/ux/botPrevent/BotPrevent.tsx
@@ -6,10 +6,11 @@ const BotPrevent: FC = () => {
     const onClickHandler = () => {
         setQuestion(getRandomQuestionData(questionData.index));
     }
+    // console.log(questionData);
     return <div>
         <h4>{questionData.question}</h4>
-        {questionData.answers.map((answer, index) => {
-            return <p key={index}>{answer.answer}</p>
+        {questionData.answers.map((answerData, index) => {
+            return <p key={index}>{answerData.answer}</p>
         })}
         <button onClick={onClickHandler}>losuj pytanie</button>
     </div>
--- src/utils/botUtils.ts
+++ src/utils/botUtils.ts
@@ -5,11 +5,22 @@ export const getRandomQuestionData = (prevIndex?: number) => {
     do {
         randomIndex = Math.floor(Math.random() * questions.length);
     } while (randomIndex === prevIndex);
-    console.log("randomIndex", randomIndex, "prevIndex", prevIndex);
 
     return {
         question: questions[randomIndex].question,
-        answers: questions[randomIndex].answers,
+        answers: shuffleAnswers([...questions[randomIndex].answers]),
         index: randomIndex
     };
+}
+interface Answers {
+    answer: string;
+    correct?: boolean;
+}
+const shuffleAnswers = (answers: Array<Answers>) => {
+    // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array/6274381#6274381
+    for (let i = answers.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+    return answers;
 }
