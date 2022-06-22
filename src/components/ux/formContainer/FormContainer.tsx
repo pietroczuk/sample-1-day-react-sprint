12f45730c8e406999e7d472b11b64f1c7cf031ab
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-22 19:00:20 +0200

    add validation icons

--- src/components/ux/formContainer/FormContainer.tsx
+++ src/components/ux/formContainer/FormContainer.tsx
@@ -1,11 +1,14 @@
 import { FC, useState, FormEvent } from "react";
 import styles from './FormContainer.module.scss';
+import ValidIcon from "../validation/validIcon/ValidIcon";
 
 const FormContainer: FC = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [message, setMessage] = useState('');
+
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
 
     const setNameHandler = (event: FormEvent<HTMLInputElement>) => {
         setName(event.currentTarget.value);
@@ -27,28 +30,45 @@ const FormContainer: FC = () => {
     }
     const formSubmitionHandler = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(name, email, phone, message);
+
+        if (name.trim() === '') {
+            setEnteredNameIsValid(false);
+            return;
+        } else {
+            setEnteredNameIsValid(true);
+        }
+        // console.log(name, email, phone, message);
     }
 
     return <div className={styles.container}>
         <form onSubmit={formSubmitionHandler}>
-            <div className={styles.inputContainer}>
+            <div className={styles.inputDataContainer}>
                 <label htmlFor="name">Imię i nazwisko</label>
-                <input id="name" type="text" onChange={setNameHandler} value={name} />
+                <div className={styles.inputContainer}>
+                    <input className={`${enteredNameIsValid ? styles.valid : styles.invalid}`} id="name" type="text" onChange={setNameHandler} value={name} />
+                    {enteredNameIsValid ?
+                        <ValidIcon isValid={true} />
+                        :
+                        <ValidIcon isValid={false} />
+                    }
+                </div>
             </div>
-            <div className={styles.inputContainer}>
+            <div className={styles.inputDataContainer}>
                 <label htmlFor="email">E-mail</label>
                 <input id="email" type="email" onChange={setEmailHandler} value={email} />
             </div>
 
-            <div className={styles.inputContainer}>
+            <div className={styles.inputDataContainer}>
                 <label htmlFor="phone">Telefon</label>
                 <input id="phone" type="phone" onChange={setPhoneHandler} value={phone} />
             </div>
-            <div className={styles.inputContainer}>
+            <div className={styles.inputDataContainer}>
                 <label htmlFor="message">Wiadomość</label>
                 <textarea id="message" rows={4} onChange={setMessageHandler} value={message} />
             </div>
+
+            <p>nie jestem robotem itd</p>
+            <p>moze jakies rodo jeszcze</p>
 
             <button className={styles.submit} type="submit">wyślij</button>
             <button onClick={resetForm}>reset</button>
---
+++ src/components/ux/validation/validIcon/ValidIcon.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import valid from '../../../ui/icons/valid.svg';
+import invalid from '../../../ui/icons/invalid.svg';
+import styles from './ValidIcon.module.scss';
+
+interface ValidIconProps {
+    isValid: boolean
+}
+
+const ValidIcon: FC<ValidIconProps> = ({ isValid }) => {
+    return <div className={styles.iconCointaner}>
+        <img width="100%" height="100%" src={isValid ? valid : invalid} alt={isValid ? "poprawne" : "niepoprawne"} />
+    </div>
+}
+
+export default ValidIcon;
