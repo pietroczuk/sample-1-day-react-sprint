f85373c7ff1ae7fff516d005060ea8ed7234740d
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-23 09:42:22 +0200

    add styles to captcha

--- src/components/ux/botPrevent/BotPrevent.tsx
+++ src/components/ux/botPrevent/BotPrevent.tsx
@@ -3,6 +3,8 @@ import { getRandomQuestionData } from "../../../utils/botUtils";
 import CheckBox from "../../ui/checkBox/CheckBox";
 import styles from './BotPrevent.module.scss';
 import refreshIcon from '../../ui/icons/refresh.svg';
+import closeIcon from '../../ui/icons/close.svg';
+import IconButton from "../iconButton/IconButton";
 
 interface BotPreventProps {
     isPassed: boolean,
@@ -21,27 +23,34 @@ const BotPrevent: FC<BotPreventProps> = ({ isPassed, onClickHandler, reset }) =>
         reset();
         setQuestion(getRandomQuestionData(questionData.index));
     }
+    const closeHandler = () => {
+        setShowCaptcha(false);
+        reset();
+    }
 
     return <div className={styles.mainCointainer}>
         <CheckBox label="Nie jestem robotem" onClick={checkboxClickHandler} />
 
         {showCaptcha && <div className={styles.captchaCointainer}>
-            <p>{isPassed ? 'HUMAN' : 'BOT'}</p>
-            <h4>{questionData.question}</h4>
-            {questionData.answers.map(answerData => {
-                const answerId = `answer_${answerData.id}_${new Date().getMilliseconds()}`;
-                return <div className={styles.answer} key={answerData.id}>
-                    <input id={answerId} type="radio" name="answer" value={answerData.id} onClick={onClickHandler} />
-                    <label htmlFor={answerId}>{answerData.answer} {answerData.id}
-                        {/* {checkAnswer(answerData.id) ? '[OK]' : ''} */}
-                    </label>
-                </div>
-            })}
+            {/* <p>{isPassed ? 'HUMAN' : 'BOT'}</p> */}
+            <div className={styles.question}>{questionData.question}</div>
+            <div className={styles.answersContainer}>
+                {questionData.answers.map(answerData => {
+                    const answerId = `answer_${answerData.id}_${new Date().getMilliseconds()}`;
+                    return <div className={styles.answer} key={answerData.id}>
+                        <input id={answerId} type="radio" name="answer" value={answerData.id} onClick={onClickHandler} />
+                        <label htmlFor={answerId}>{answerData.answer} {answerData.id}
+                            {/* {checkAnswer(answerData.id) ? '[OK]' : ''} */}
+                        </label>
+                    </div>
+                })}
+            </div>
             <div className={styles.buttonsCointaner}>
-            <button onClick={()=> {}}>sprawdź</button>
-                <div className={styles.refresh} onClick={randomOnClickHandler}>
-                    <img src={refreshIcon} alt="odśwież" width="100%" height="100%" />
-                </div>   
+                <button className={styles.send} onClick={() => { }}>sprawdź odpowiedź</button>
+                <IconButton iconSrc={refreshIcon} onClick={randomOnClickHandler} />
+            </div>
+            <div className={styles.closeIconCointaner}>
+                <IconButton iconSrc={closeIcon} onClick={closeHandler} />
             </div>
         </div>}
     </div>
