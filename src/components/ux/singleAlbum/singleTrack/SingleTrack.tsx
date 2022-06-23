77b7820cf274aec401f54cf9d700ffd52af3ca93
andrzej pietroczuk <andrzej@pietroczuk.pl>
2022-06-23 16:41:15 +0200

    add fake play buttons to tracks

--- src/components/ux/singleAlbum/singleTrack/SingleTrack.tsx
+++ src/components/ux/singleAlbum/singleTrack/SingleTrack.tsx
@@ -1,13 +1,27 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from './SingleTrack.module.scss';
+import playIcon from '../../../ui/icons/play.svg';
+import pauseIcon from '../../../ui/icons/pause.svg';
 
 interface SingleTrackProps {
     trackName: string;
 }
 
 const SingleTrack: FC<SingleTrackProps> = ({ trackName }) => {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const onClickHandler = () => {
+        setIsPlaying(prev => !prev);
+    }
+
     return <div className={styles.trackContainer}>
-        {trackName}
+        <div className={`${styles.icon} ${isPlaying ? styles.active : ''}`} onClick={onClickHandler} >
+            <img src={isPlaying ? pauseIcon : playIcon} alt="posłuchaj" width="100%" height="100%" />
+        </div>
+        <div className={styles.name}>
+            {trackName}
+        </div>
     </div>
 }
 
